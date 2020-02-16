68a68600421b78c0247020a38f21b973d2f5775e
jkwebco <jkwebdesignco@gmail.com>
2020-02-15 19:58:36 -0500

    changed settings.py

--- hnclone/settings.py
+++ hnclone/settings.py
@@ -190,7 +190,7 @@ SITE_DOMAIN = 'aiwebnews.herokuapp.com'
 
 PROJECT_ROOT   =   os.path.join(os.path.abspath(__file__))
 STATIC_ROOT  =   os.path.join(PROJECT_ROOT, 'staticfiles')
-STATIC_URL = '/static/'
+STATIC_URL = 'static/'
 
 # Extra lookup directories for collectstatic to find static files
 STATICFILES_DIRS = (
